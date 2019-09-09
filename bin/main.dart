ed82f2eb539c665e17248dec2b65253c0594d1e7
Bryan Cairns <bcairns@voidrealms.com>
2019-09-09 07:20:53 -0400

    UTF8 renamed to utf8

--- bin/main.dart
+++ bin/main.dart
@@ -5,9 +5,14 @@ main(List<String> arguments) {
 
   //Linux Specific
   Process.start('cat', []).then((Process process) { //console DIR
-    //transform the output
-    process.stdout.transform(UTF8.decoder).listen((data) {print(data);});
-
+    
+    
+    //Older Dart
+    //process.stdout.transform(UTF8.decoder).listen((data) {print(data);});
+    
+    //Newer dart
+    process.stdout.transform(utf8.decoder).listen((data) {print(data);});
+    
     //Send to the process
     process.stdin.writeln('Hello World');
 
